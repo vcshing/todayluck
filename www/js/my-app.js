cd292fc53a0f4c4402d585f9997885cb803e3451
Skyler Liu <vcshing2005@hotmail.com>
2017-03-04 20:40:37 +0800

    asdasd

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,27 +14,27 @@ var mainView = myApp.addView('.view-main', {
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
-	
-   // Set AdMobAds options: 
+
+   // Set AdMobAds options:
   admob.setOptions({
-	publisherId:          "ca-app-pub-3715336230214756/5679557820",  // Required 
-	interstitialAdId:     "ca-app-pub-3715336230214756/7156291028",  // Optional 
-	tappxIdAndroid:       "/120940746/Pub-14960-Android-6916",        // Optional 
+	publisherId:          "ca-app-pub-3715336230214756/3466501023",  // Required
+	interstitialAdId:     "ca-app-pub-3715336230214756/4943234225",  // Optional
+	tappxIdAndroid:       "/120940746/Pub-15267-Android-0348",        // Optional
 	tappxShare:           0.1,
 	isTesting: false, // receiving test ads (do not test with real ads as your account will be banned)				// Optional
 	bannerAtTop: false, // set to true, to put banner at top
 	overlap: true, // set to true, to allow banner overlap webview
 	offsetStatusBar: false, // set to true to avoid ios7 status bar overlap
 	autoShowBanner: true, // auto show banners ad when loaded
-	autoShowInterstitial: true // auto show interstitials ad when loaded	// Optional 
+	autoShowInterstitial: true // auto show interstitials ad when loaded	// Optional
   });
-  
-  // Start showing banners (atomatic when autoShowBanner is set to true) 
+
+  // Start showing banners (atomatic when autoShowBanner is set to true)
   admob.createBannerView();
-  
-  // Request interstitial (will present automatically when autoShowInterstitial is set to true) 
+
+  // Request interstitial (will present automatically when autoShowInterstitial is set to true)
   //admob.requestInterstitialAd();
-    
+
 	//navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
 });
 
@@ -51,23 +51,28 @@ myApp.onPageInit('about', function (page) {
 $$(document).on('pageInit', function (e) {
     // Get page data from event data
     var page = e.detail.page;
-
+console.log(page.name);
     if (page.name === 'about') {
         // Following code will be executed for page with data-page attribute equal to "about"
-        myApp.alert('Here comes About page');
+       // myApp.alert('1Here comes About page');
     }
 })
 
 // Option 2. Using live 'pageInit' event handlers for each page
-$$(document).on('pageInit', '.page[data-page="about"]', function (e) {
+$$(document).on('pageInit', '.page[data-page="twelveConstellationsDetail"]', function (e) {
     // Following code will be executed for page with data-page attribute equal to "about"
-    myApp.alert('Here comes About page');
+    //myApp.alert('2Here comes About page');
+	//alert(123);
+
+	//$(".twelveConstellationsDetailContents").html(toCode($(".twelveConstellationsDetailContents").html(),1));
+
+
 })
 
 
 $$(document).on('pause', function (e) {
 	if($(".onoffswitch-checkbox2").is(':checked')=== true){
-		
+
 	}else{
 		navigator.vibrate([]);
 		window.clearInterval(IntervalVibrate);
@@ -79,13 +84,11 @@ $$(document).on('pause', function (e) {
 
 $$(document).on('resume', function (e) {
 	if(($(".onoffswitch-checkbox").is(':checked')=== true)){
-		navigator.vibrate([1000, 1000, 3000, 1000, 5000]); 
+		navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
 		if(IntervalVibrate==""){
-			IntervalVibrate = window.setInterval(function(){ 	
-				navigator.vibrate([1000, 1000, 3000, 1000, 5000]); 
-			},11000);	
+			IntervalVibrate = window.setInterval(function(){
+				navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
+			},11000);
 		}
 	}
 })
-
-
